5c4eaf32643f938ea06cae0d309338e8184833a4
nanalemon <86542818+nanalemon@users.noreply.github.com>
2022-04-19 00:13:08 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@
   const btn=document.getElementById('btn');
 
   btn.addEventListener('click',()=>{
-    const results=['Vegetable','Fish','Meat','Fruit','Rice','Bread','Soy','Cheese','Cake','Chocolate','Cookie','Olive','Egg','Beef','Tomato','Broccoli','Noodles','Potato','Beer','Shrimp','Mochi','Sandwich'];
+    const results=['Vegetable','Fish','Meat','Fruit','Rice','Bread','Soy','Cheese','Cake','Chocolate','Cookie','Olive','Egg','Beef','Tomato','Broccoli','Noodles','Potato','Beer','Shrimp','Mochi','Sandwich','Pork','Chicken','Pizza','Sushi','Croquette','Ice cream','Matcha','Tempura','Curry'];
 
     btn.textContent=results[Math.floor(Math.random()*results.length)];
   });
